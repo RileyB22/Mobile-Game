714577170f2c67369e1fd4a6e666ddb012e5377d
RileyB22 <147097462+RileyB22@users.noreply.github.com>
2025-05-25 21:03:46 -0500

    chapter 3

--- My project/Assets/Scripts/ObstacleBehaviour.cs
+++ My project/Assets/Scripts/ObstacleBehaviour.cs
@@ -29,4 +29,24 @@ public class ObstacleBehaviour : MonoBehaviour
         // Restarts the current level
         SceneManager.LoadScene(sceneName);
     }
+
+    [Tooltip("Explosion effect to play when tapped")]
+    public GameObject explosion;
+
+    /// <summary>
+    /// If the object is tapped, we spawn an explosion and
+    /// destroy this object
+    /// </summary>
+    private void PlayerTouch()
+    {
+        if (explosion != null)
+        {
+            var particles = Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(particles, 1.0f);
+        }
+        Destroy(this.gameObject);
+
+        // there is extra but notes say no add
+    }
+
 }
